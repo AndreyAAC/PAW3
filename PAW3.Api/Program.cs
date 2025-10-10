4b2429fc4c18d36de9177275336877d685a6652d
Andrey Aguilar Chaves <acaguilar@CLTBCANADA.net>
2025-10-10 12:24:43 -0400

    CRUD de USER completado

--- PAW3.Api/Program.cs
+++ PAW3.Api/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddScoped<IInventoryBusiness, InventoryBusiness>();
 builder.Services.AddScoped<IRepositoryInventory, RepositoryInventory>();
 builder.Services.AddScoped<IComponentBusiness, ComponentBusiness>();
 builder.Services.AddScoped<IRepositoryComponent, RepositoryComponent>();
+builder.Services.AddScoped<IUserBusiness, UserBusiness>();
+builder.Services.AddScoped<IRepositoryUser, RepositoryUser>();
 
 var app = builder.Build();
 
---
+++ PAW3.Core/BusinessLogic/UserBusiness.cs
@@ -0,0 +1,51 @@
+﻿using PAW3.Data.Models;
+using PAW3.Data.Repositories;
+
+namespace PAW3.Core.BusinessLogic;
+
+public interface IUserBusiness
+{
+    Task<bool> DeleteUserAsync(int id);
+    Task<IEnumerable<User>> GetUsers(int? id);
+    Task<bool> SaveUserAsync(User user);
+}
+
+public class UserBusiness(IRepositoryUser repositoryUser) : IUserBusiness
+{
+    public async Task<bool> SaveUserAsync(User user)
+    {
+        var entity = await repositoryUser.FindAsync(user.UserId);
+
+        if (entity == null)
+        {
+            user.CreatedAt ??= DateTime.UtcNow;
+            user.LastModified ??= DateTime.UtcNow;
+            return await repositoryUser.CreateAsync(user);
+        }
+        else
+        {
+            entity.Username = user.Username;
+            entity.Email = user.Email;
+            entity.PasswordHash = user.PasswordHash;
+            entity.IsActive = user.IsActive;
+            entity.LastModified = user.LastModified ?? DateTime.UtcNow;
+            entity.ModifiedBy = user.ModifiedBy;
+            entity.RoleId = user.RoleId;
+
+            return await repositoryUser.UpdateAsync(entity);
+        }
+    }
+
+    public async Task<bool> DeleteUserAsync(int id)
+    {
+        var entity = await repositoryUser.FindAsync(id);
+        return entity is null ? false : await repositoryUser.DeleteAsync(entity);
+    }
+
+    public async Task<IEnumerable<User>> GetUsers(int? id)
+    {
+        return id is null
+            ? await repositoryUser.ReadAsync()
+            : [await repositoryUser.FindAsync((int)id)];
+    }
+}
